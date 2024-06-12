38a1fea4337bd839e29261f89553ab0cce85124d
Ruslan <Giovingo92@gmail.com>
2024-06-12 20:40:55 +0300

    feat: auth session cookies

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -18,7 +18,7 @@ const lucia = new Lucia(adapter, {
   },
 });
 
-export async function createAuthSession(userId: string) {
+export async function createAuthSession(userId: any) {
   const session = await lucia.createSession(userId, {});
   const sessionCookie = lucia.createSessionCookie(session.id);
   cookies().set(
